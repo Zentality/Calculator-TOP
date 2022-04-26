0024541165f0293ad6b1a26796b74e479a16602f
Zentality <jmcp@live.com.au>
2022-04-26 20:28:44 +0800

    Add styling to calculator

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-// function multiply(){
-//   total = 1;
-//   for (let i = 0; i < arguments.length; i++){
-//     total *= arguments[i];
-//   }
-//   return total;
-// }
-
-// function subtract(first, second){
-//   return first - second;
-// }
-
-// function add(){
-//   total = 0;
-//   for (let i = 0; i < arguments.length; i++){
-//     total += arguments[i];
-//   }
-//   return total;
-// }
-
-// function divide(first, second){
-//   return first / second;
-// }
-
 const operations = {
   add: (first, second) => first + second,
   subtract: (first, second) => first - second,
