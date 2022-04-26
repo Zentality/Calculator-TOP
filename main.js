8964ab25bc5388d68783d3d6a2a25bb0ccfc572a
Zentality <jmcp@live.com.au>
2022-04-26 19:04:30 +0800

    Make operators smaller and put into an object

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
-function multiply(){
-  total = 1;
-  for (let i = 0; i < arguments.length; i++){
-    total *= arguments[i];
-  }
-  return total;
+// function multiply(){
+//   total = 1;
+//   for (let i = 0; i < arguments.length; i++){
+//     total *= arguments[i];
+//   }
+//   return total;
+// }
+
+// function subtract(first, second){
+//   return first - second;
+// }
+
+// function add(){
+//   total = 0;
+//   for (let i = 0; i < arguments.length; i++){
+//     total += arguments[i];
+//   }
+//   return total;
+// }
+
+// function divide(first, second){
+//   return first / second;
+// }
+
+const operations = {
+  add: (first, second) => first + second,
+  subtract: (first, second) => first - second,
+  multiply: (first, second) => first * second,
+  divide: (first, second) => first / second,
 }
 
-function subtract(first, second){
-  return first - second;
-}
-
-function add(){
-  total = 0;
-  for (let i = 0; i < arguments.length; i++){
-    total += arguments[i];
-  }
-  return total;
-}
-
-function divide(first, second){
-  return first / second;
+function calc(operater, first, second){
+  return operations[operater](first, second);
 }
