83c832f661042118d9a2bb1712e9e1abf1049967
Zentality <jmcp@live.com.au>
2022-04-27 09:50:59 +0800

    Fix logic, equals clears field and returns value

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Global Variables
-let totalValue = "";
+let totalValue = 0;
 let currentValue = "";
 let firstValue = 0;
 let secondValue = 0;
@@ -9,18 +9,34 @@ let operator = "";
 
 //Query selectors
 const numberButtons = document.querySelectorAll(".numberButtons");
-const operatorButtons = document.querySelectorAll(".operatorButtons")
+const operatorButtons = document.querySelectorAll(".operatorButtons");
+const equalsButton = document.querySelector("#equals")
 const screenCurrent = document.querySelector("#screenCurrent");
+const screenTotal = document.querySelector("#screenTotal");
 
 
 
 //Event Listeners
 numberButtons.forEach((button) => {
-  button.addEventListener("click", (e) => appendCurrent(button.textContent));
+  button.addEventListener("click", () => {
+    currentValue += button.textContent;
+    screenCurrent.textContent += button.textContent;
+    console.log(currentValue, firstValue, secondValue, operator);
+  });
 });
 
 operatorButtons.forEach((button) => {
-  button.addEventListener("click", (e) => appendOperator(button.textContent))
+  button.addEventListener("click", () => {
+    appendOperator(button.textContent);
+    console.log(currentValue, firstValue, secondValue, operator);
+  });
+});
+
+equalsButton.addEventListener("click", () => {
+  secondValue = parseInt(currentValue);
+  totalValue = operations[operator](firstValue,secondValue);
+  screenTotal.textContent = totalValue;
+  clear();
 });
 
 
@@ -33,31 +49,30 @@ const operations = {
   divide: (first, second) => first / second,
 }
 
-
-
-//Functions
-function appendCurrent(value){
-  currentValue += value;
-  screenCurrent.textContent = currentValue;
-}
-
 function appendOperator(value){
   firstValue = parseInt(currentValue);
   currentValue = "";
-  screenCurrent.textContent = "";
+  screenCurrent.textContent += ` ${value} `;
+  console.log(value);
   switch(value){
     case "+":
       operator = "add";
+      break;
     case "-":
       operator = "subtract";
+      break;
     case "x":
       operator = "multiply";
+      break;
     case "÷":
-    operator = "divide";
+      operator = "divide";
+      break;
   }
 }
 
-function calc(operater, first, second){
-  return operations[operater](first, second);
+function clear(){
+  screenCurrent.textContent = "";
+  currentValue = "";
+  firstValue = 0;
+  secondValue = 0;
 }
-
